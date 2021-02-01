c87e49bb95383ae9398fbadc2b9044a033b42fa0
melon <jack716@naver.com>
2021-02-01 23:53:04 +0900

    test

---
+++ srcs/mysql/srcs/mysql.sh
@@ -0,0 +1,6 @@
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
+	| mysql -u root --skip-password &&\
+	echo "CREATE USER IF NOT EXISTS 'hroh'@'%' IDENTIFIED BY 'hroh';" \
+	| mysql -u root --skip-password &&\
+	echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'hroh'@'%' WITH GRANT OPTION;" \
+	| mysql -u root --skip-password
